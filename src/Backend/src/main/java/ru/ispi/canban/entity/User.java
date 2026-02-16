222c3ac71b8da0908528a9d9e9d6b380cc7e92cf
Roman <142125179+r0mbaa@users.noreply.github.com>
2026-02-16 12:18:07 +0300

    fix

--- src/Backend/src/main/java/ru/ispi/canban/entity/User.java
+++ src/Backend/src/main/java/ru/ispi/canban/entity/User.java
@@ -1,34 +1,35 @@
 package ru.ispi.canban.entity;
 
-//import jakarta.persistence.*;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-//@Entity
-//@Table(name = "users")
+@Entity
+@Table(name = "users")
 @Getter
 @Setter
-public class User{
+public class User {
 
-    //@Id
-    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    //@Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
-    //@Column(nullable = false)
+    @Column(nullable = false)
     private String name;
 
-    //@Column(name = "password_hash", nullable = false)
+    @Column(name = "password_hash", nullable = false)
     private String passwordHash;
 
-    //@Column(name = "avatar_url")
+    @Column(name = "avatar_url")
     private String avatarUrl;
 
-    //@Column(name = "created_at", insertable = false, updatable = false)
+    @Column(name = "created_at",
+            insertable = false,
+            updatable = false)
     private LocalDateTime createdAt;
 }
-
---
+++ src/Backend/src/main/java/ru/ispi/canban/repository/MySqlUserRepositoryImpl.java
@@ -0,0 +1,35 @@
+package ru.ispi.canban.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.ispi.canban.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MySqlUserRepositoryImpl implements UserRepository {
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public Optional<User> FindById(Integer id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> FindByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public void deleteById(Integer id) {
+
+    }
+}
